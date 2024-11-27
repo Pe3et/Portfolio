7cc399e9c0e3096bcd29b7b3547bea166a66e3fb
Pe3et <wolfpeeet@googlemail.com>
2024-11-27 17:17:33 +0100

    continued working on responisve header

--- src/app/hero/header/header.component.ts
+++ src/app/hero/header/header.component.ts
@@ -7,21 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
+  respLogoFull = new Image()
+  respLogoHover = new Image()
   logoFull = new Image()
   logoHover = new Image()
-  currentLogo: HTMLImageElement
+  currentRespLogo: HTMLImageElement = this.respLogoFull
+  currentLogo: HTMLImageElement = this.logoFull
+
 
   constructor() {
-    this.logoFull.src = 'assets/img/logo.png'
-    this.logoHover.src = 'assets/img/logo_hover.png'
-    this.currentLogo = this.logoFull
+    this.respLogoFull.src = 'assets/img/logo.png'
+    this.respLogoHover.src = 'assets/img/logo_hover.png'
+    this.logoFull.src = 'assets/img/logo_full.png'
+    this.logoHover.src = 'assets/img/logo_full_hover.png'
   }
 
   logoHoverChange() {
+    this.currentRespLogo = this.respLogoHover
     this.currentLogo = this.logoHover
   }
 
   logoFullChange() {
+    this.currentRespLogo = this.respLogoFull
     this.currentLogo = this.logoFull
   }
 }
