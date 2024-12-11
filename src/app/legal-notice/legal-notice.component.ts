cd036c05c8df65b256a3efbc1ddb11c1be7540b5
Pe3et <wolfpeeet@googlemail.com>
2024-12-11 09:53:11 +0100

    added german legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TextContentService } from '../shared/text-content/text-content.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.sass'
 })
 export class LegalNoticeComponent {
-
+  text = inject(TextContentService)
 }
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TextContentService } from '../shared/text-content/text-content.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.sass'
 })
 export class PrivacyPolicyComponent {
-
+  text = inject(TextContentService)
 }
