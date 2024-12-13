6a6ff1a1c8037aeb8b27f7911fa51c1de9193bd2
Pe3et <wolfpeeet@googlemail.com>
2024-12-13 11:42:11 +0100

    project overlay button linking

--- src/app/main-page/portfolio/portfolio.component.ts
+++ src/app/main-page/portfolio/portfolio.component.ts
@@ -10,7 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class PortfolioComponent {
   text = inject(TextContentService)
-  projects: {title: string, skills: string[], previewSrc: string , description: {EN: string, DE: string}} [] = [
+  projects: {
+    title: string,
+    skills: string[],
+    previewSrc: string ,
+    description: {EN: string, DE: string},
+    gitHub: string,
+    live: string
+    } [] = [
     {
       title: 'Join',
       skills: ['JavaScript', 'HTML', 'CSS', 'Firebase'],
@@ -18,7 +25,9 @@ export class PortfolioComponent {
       description: {
         EN: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
         DE: 'Aufgabenverwaltungssystem inspiriert vom Kanban-Prinzip. Ermöglicht das Erstellen und Organisieren von Aufgaben mittels Drag-and-Drop-Funktionalität sowie die Zuweisung von Benutzern und Kategorien.'
-      }
+      },
+      gitHub: 'https://github.com/Pe3et/Join',
+      live: 'https://join.peeet.net'
     },
     {
       title: 'Demon Assault',
@@ -27,7 +36,9 @@ export class PortfolioComponent {
       description: {
         EN: `Challenging Jump 'n' Run game based on object-oriented programming. Collect droppables and shoot lightnings.`,
         DE: `Herausforderndes Jump 'n' Run-Spiel basierend auf objektorientierter Programmierung. Sammle fallengelassene Items und schieße Blitze.`
-      }
+      },
+      gitHub: 'https://github.com/Pe3et/DA---Demon-Assault',
+      live: 'https://demon-assault.peeet.net'
     }
   ]
   overlayVisibility: boolean = false
